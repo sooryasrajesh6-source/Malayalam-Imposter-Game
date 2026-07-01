dba12dd85de483e7e5be4bc9e9bfcac2aa75b2ff
Soorya S Rajesh <sooryasrajesh6@gmail.com>
2026-07-01 20:11:31 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,39 @@
-const CACHE_NAME = "malayalam-imposter-v1";
+const CACHE_NAME = "malayalam-imposter-v4";
 
 const filesToCache = [
   "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./service-worker.js",
+  "./icon.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(filesToCache))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
