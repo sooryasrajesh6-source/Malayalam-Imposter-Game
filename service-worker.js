852ca662071f96b5bb531788a8cc7730f3763aac
Soorya S Rajesh <sooryasrajesh6@gmail.com>
2026-07-01 21:07:55 +0530

    Update cache name and optimize service worker code

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "malayalam-imposter-v7";
+const CACHE_NAME = "malayalam-imposter-v10";
 
 const filesToCache = [
   "./",
@@ -7,34 +7,23 @@ const filesToCache = [
   "./script.js",
   "./words.js",
   "./manifest.json",
-  "./service-worker.js",
-  "./icon.png"
+  "./service-worker.js"
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(filesToCache))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(filesToCache)));
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if(cache !== CACHE_NAME){
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
+    caches.keys().then(cacheNames =>
+      Promise.all(cacheNames.map(cache => cache !== CACHE_NAME ? caches.delete(cache) : null))
+    )
   );
   self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
