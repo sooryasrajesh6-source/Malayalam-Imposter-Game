6f2ce72e301e97e4245ae7a7d1520fc2d219b2ed
Soorya S Rajesh <sooryasrajesh6@gmail.com>
2026-07-01 20:25:14 +0530

    Add service worker for caching files

---
+++ script.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "malayalam-imposter-v6";
+
+const filesToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./words.js",
+  "./manifest.json",
+  "./service-worker.js",
+  "./icon.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(filesToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if(cache !== CACHE_NAME){
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
